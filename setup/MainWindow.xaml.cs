56f49204d48281273a2289b7065efac07d0d7181
Awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2022-05-26 22:49:11 -0400

    Installer mostly complete

--- setup/MainWindow.xaml.cs
+++ setup/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,14 +15,32 @@ using System.Windows.Shapes;
 
 namespace setup
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        
+        private void Install_Click(object sender, RoutedEventArgs e)
+        {
+            if (AgreeBox.IsChecked == true)
+            {
+                MessageBox.Show("Installation complete!");
+            }
+            else
+            {
+                MessageBox.Show("You must agree by checking the box to continue.");
+            }    
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to cancel?", "WinMin", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                Application.Current.Shutdown();
+            }
         }
     }
 }
