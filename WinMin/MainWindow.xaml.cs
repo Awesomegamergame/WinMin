0a22f3fe22a5769f6c50547c00167c2a828260e5
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2022-11-17 13:46:54 -0500

    Part 1 of loading the file types

--- WinMin/MainWindow.xaml.cs
+++ WinMin/MainWindow.xaml.cs
@@ -8,6 +8,9 @@ using System.Windows.Controls;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Windows.Media.Imaging;
+using Newtonsoft.Json;
+using System.IO.Compression;
 
 namespace WinMin
 {
@@ -28,6 +31,7 @@ namespace WinMin
             InitializeComponent();
             if (Updater.IsOnline) { Updater.Update(); }
             RegistryChanger.CreateJson();
+            #region WinMin wmpatch Extension
             if (File.Exists("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk"))
             {
                 string userName = File.ReadAllText($"C:\\Users\\Public\\WinMin\\UserName.txt");
@@ -57,6 +61,7 @@ namespace WinMin
                 process.WaitForExit();
                 SHChangeNotify(0x08000000, 0x0000, IntPtr.Zero, IntPtr.Zero);
             }
+            #endregion
         }
 
         private void RegB_Click(object sender, RoutedEventArgs e)
@@ -121,6 +126,7 @@ namespace WinMin
             RegistryChanger.LoadUserRegistry(Settings, "SettingsPageVisibility", explorerKey);
             RegistryChanger.LoadUserRegistry(RightClick, "NoViewContextMenu", explorerKey);
             RegistryChanger.LoadUserRegistry(RightClickTask, "NoTrayContextMenu", explorerKey);
+            WMTest();
         }
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
@@ -166,6 +172,152 @@ namespace WinMin
                 process.WaitForExit();
                 MessageBox.Show("Registry file loaded");
             }
+        }
+        public void WMTest()
+        {
+            if (!Directory.Exists($"{rootPath}\\Patches"))
+                Directory.CreateDirectory($"{rootPath}\\Patches");
+            if (!Directory.Exists($"{rootPath}\\Patches\\Installed"))
+                Directory.CreateDirectory($"{rootPath}\\Patches\\Installed");
+
+            foreach (string file in Directory.GetFiles($"{rootPath}\\Patches"))
+            {
+                ZipArchive archive = ZipFile.OpenRead(file);
+                var sample = archive.GetEntry("manifest.json");
+                Stream zipEntryStream = sample.Open();
+                StreamReader reader = new StreamReader(zipEntryStream);
+                string json = reader.ReadToEnd();
+                WMManifest manifest = JsonConvert.DeserializeObject<WMManifest>(json);
+                if (manifest.supportedVersions.Contains(manifest.manifest))
+                {
+                    Directory.CreateDirectory($"{rootPath}\\Patches\\Installed\\{manifest.name}");
+                    try
+                    {
+                        archive.ExtractToDirectory($"{rootPath}\\Patches\\Installed\\{manifest.name}");
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("This plugin already exists hit remove for the plugin if you are trying to update it");
+                    }
+                    archive.Dispose();
+                    Grid grid = new Grid
+                    {
+                        Height = 120,
+                        Width = 406,
+                        HorizontalAlignment = HorizontalAlignment.Left
+                    };
+                    StackPanel panel = new StackPanel
+                    {
+                        Width = 310,
+                        HorizontalAlignment = HorizontalAlignment.Right
+                    };
+
+                    TextBlock text = new TextBlock
+                    {
+                        Height = 20,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        Text = "Name: " + manifest.name
+                    };
+                    panel.Children.Add(text);
+
+                    TextBlock text2 = new TextBlock
+                    {
+                        Height = 20,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        Text = "Version: " + manifest.version
+                    };
+                    panel.Children.Add(text2);
+
+                    TextBlock text3 = new TextBlock
+                    {
+                        Height = 20,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        Text = "Author: " + manifest.author
+                    };
+                    panel.Children.Add(text3);
+
+                    TextBlock text4 = new TextBlock
+                    {
+                        Height = 52,
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        TextWrapping = TextWrapping.WrapWithOverflow,
+                        Text = "Description: " + manifest.description
+                    };
+                    panel.Children.Add(text4);
+
+                    StackPanel panel2 = new StackPanel
+                    {
+                        Width = 90,
+                        HorizontalAlignment = HorizontalAlignment.Left
+                    };
+                    BitmapImage bitmap = new BitmapImage();
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri($"{rootPath}\\Patches\\Installed\\{manifest.name}\\{manifest.cover}", UriKind.RelativeOrAbsolute);
+                    bitmap.EndInit();
+                    Image image = new Image
+                    {
+                        Source = bitmap,
+                        Height = 90,
+                        Stretch = System.Windows.Media.Stretch.Uniform
+                    };
+                    panel2.Children.Add(image);
+
+                    Button button = new Button
+                    {
+                        VerticalAlignment = VerticalAlignment.Bottom,
+                        Content = "Remove",
+                        Tag = manifest.name,
+                        Height = 30
+                    };
+                    panel2.Children.Add(button);
+
+                    grid.Children.Add(panel);
+                    grid.Children.Add(panel2);
+                    WMInstalled.Items.Add(grid);
+                    File.Delete(file);
+
+                    //Load all of the keys from the wmpatch file into the registry
+                    foreach (string keyPath in manifest.patchFiles)
+                    {
+                        MessageBox.Show($"/C reg import \"{rootPath}\\Patches\\Installed\\{manifest.name}\\{keyPath}\"");
+                        Process process = new Process();
+                        ProcessStartInfo startInfo = new ProcessStartInfo
+                        {
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            FileName = "cmd.exe",
+                            Arguments = $"/C reg import \"{rootPath}\\Patches\\Installed\\{manifest.name}\\{keyPath}\""
+                        };
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        process.WaitForExit();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Manifest Version Not Supported");
+                }
+                
+            }
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            PatchesMenu.Visibility = Visibility.Collapsed;
+            MenuButton.Visibility = Visibility.Collapsed;
+            APatches.Visibility = Visibility.Collapsed;
+            IPatches.Visibility = Visibility.Collapsed;
+            WMAvailable.Visibility = Visibility.Collapsed;
+            WMInstalled.Visibility = Visibility.Collapsed;
+        }
+
+        private void WMPatchB_Click(object sender, RoutedEventArgs e)
+        {
+            PatchesMenu.Visibility = Visibility.Visible;
+            MenuButton.Visibility = Visibility.Visible;
+            APatches.Visibility = Visibility.Visible;
+            IPatches.Visibility = Visibility.Visible;
+            WMAvailable.Visibility = Visibility.Visible;
+            WMInstalled.Visibility = Visibility.Visible;
         }
     }
 }
