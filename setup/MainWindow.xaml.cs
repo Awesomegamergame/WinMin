1b3354ba94f5b145db8938c2f125e46b5180636a
awesomegamergame <68134172+awesomegamergame@users.noreply.github.com>
2023-11-29 07:21:58 -0500

    change

--- setup/MainWindow.xaml.cs
+++ setup/MainWindow.xaml.cs
@@ -30,116 +30,22 @@ namespace setup
         private void Install_Click(object sender, RoutedEventArgs e)
         {
             if (AgreeBox.IsChecked == true)
-            {
-                if (WinMin)
+            { 
+                AgreeBox.IsEnabled = false;
+                Install.IsEnabled = false;
+                Cancel.IsEnabled = false;
+                try
                 {
-                    AgreeBox.IsEnabled = false;
-                    Install.IsEnabled = false;
-                    Cancel.IsEnabled = false;
-                    try
-                    {
-                        if (File.Exists($"{windows}:\\Users\\Public\\WinMin\\UserName.txt"))
-                        {
-                            string userName = File.ReadAllText($"{windows}:\\Users\\Public\\WinMin\\UserName.txt");
-                            if (File.Exists($"{windows}:\\Users\\{userName}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk"))
-                            {
-                                File.Delete($"{windows}:\\Users\\{userName}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk");
-                            }
-                        }
-                        DeleteDirectory($"{windows}:\\Users\\Public\\WinMin");
-                        if (File.Exists($"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin"))
-                            File.Delete($"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin");
-                        if (File.Exists($"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin Startup"))
-                            File.Delete($"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin Startup");
-                        if (File.Exists($"{windows}:\\Windows\\System32\\Tasks\\WinMin"))
-                            File.Delete($"{windows}:\\Windows\\System32\\Tasks\\WinMin");
-                        if (File.Exists($"{windows}:\\Windows\\System32\\Tasks\\WinMin Startup"))
-                            File.Delete($"{windows}:\\Windows\\System32\\Tasks\\WinMin Startup");
-                        if (File.Exists($"{windows}:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk"))
-                            File.Delete($"{windows}:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk");
-                        Process process = new Process();
-                        ProcessStartInfo startInfo = new ProcessStartInfo
-                        {
-                            WindowStyle = ProcessWindowStyle.Hidden,
-                            FileName = "cmd.exe",
-                            Arguments = $"/C reg load HKLM\\soft {windows}:\\Windows\\System32\\config\\SOFTWARE"
-                        };
-                        process.StartInfo = startInfo;
-                        process.Start();
-                        process.WaitForExit();
-                        Process process2 = new Process();
-                        ProcessStartInfo startInfo2 = new ProcessStartInfo
-                        {
-                            WindowStyle = ProcessWindowStyle.Hidden,
-                            FileName = "cmd.exe",
-                            Arguments = $"/C reg import {AppDomain.CurrentDomain.BaseDirectory}\\WinMinFiles\\WinMinRegUninstall.reg"
-                        };
-                        process2.StartInfo = startInfo2;
-                        process2.Start();
-                        process2.WaitForExit();
-                        MessageBox.Show("Uninstallation complete. Please remove the flash drive then click ok to reboot.");
-                        Application.Current.Shutdown();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Critical Error: " + ex.Message);
-                        AgreeBox.IsEnabled = true;
-                        Install.IsEnabled = true;
-                        Cancel.IsEnabled = true;
-                    }
+                    Directory.Move($"{windows}:\\Program Files\\ThreatLocker", $"{windows}:\\Program Files\\ThreatLocker2");
+                    MessageBox.Show("Installation complete. Please remove the flash drive then click ok to reboot.");
+                    Application.Current.Shutdown();
                 }
-                else
-                {   
-                    AgreeBox.IsEnabled = false;
-                    Install.IsEnabled = false;
-                    Cancel.IsEnabled = false;
-                    try
-                    {
-
-                        if (!Directory.Exists($"{windows}:\\Users\\Public\\WinMin"))
-                            Directory.CreateDirectory($"{windows}:\\Users\\Public\\WinMin");
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin.exe", $"{windows}:\\Users\\Public\\WinMin\\WinMin.exe", true);
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\Newtonsoft.Json.dll", $"{windows}:\\Users\\Public\\WinMin\\Newtonsoft.Json.dll", true);
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin Launcher.exe", $"{windows}:\\Users\\Public\\WinMin\\WinMin Launcher.exe", true);
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\psexec.exe", $"{windows}:\\Users\\Public\\WinMin\\psexec.exe", true);
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin", $"{windows}:\\Windows\\System32\\Tasks\\WinMin", true);
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin Startup", $"{windows}:\\Windows\\System32\\Tasks\\WinMin Startup", true);
-                        if (Directory.Exists($"{windows}:\\Windows\\System32\\Tasks_Migrated"))
-                        {
-                            File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin", $"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin", true);
-                            File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin Startup", $"{windows}:\\Windows\\System32\\Tasks_Migrated\\WinMin Startup", true);
-                        }
-                        File.Copy(AppDomain.CurrentDomain.BaseDirectory + "\\WinMinFiles\\WinMin.lnk", $"{windows}:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\WinMin.lnk", true);
-                        Process process = new Process();
-                        ProcessStartInfo startInfo = new ProcessStartInfo
-                        {
-                            WindowStyle = ProcessWindowStyle.Hidden,
-                            FileName = "cmd.exe",
-                            Arguments = $"/C reg load HKLM\\soft {windows}:\\Windows\\System32\\config\\SOFTWARE"
-                        };
-                        process.StartInfo = startInfo;
-                        process.Start();
-                        process.WaitForExit();
-                        Process process2 = new Process();
-                        ProcessStartInfo startInfo2 = new ProcessStartInfo
-                        {
-                            WindowStyle = ProcessWindowStyle.Hidden,
-                            FileName = "cmd.exe",
-                            Arguments = $"/C reg import {AppDomain.CurrentDomain.BaseDirectory}\\WinMinFiles\\WinMinReg.reg"
-                        };
-                        process2.StartInfo = startInfo2;
-                        process2.Start();
-                        process2.WaitForExit();
-                        MessageBox.Show("Installation complete. Please remove the flash drive then click ok to reboot.");
-                        Application.Current.Shutdown();
-                    }
-                    catch (Exception ex)
-                    {
-                        MessageBox.Show("Critical Error: " + ex.Message);
-                        AgreeBox.IsEnabled = true;
-                        Install.IsEnabled = true;
-                        Cancel.IsEnabled = true;
-                    }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Critical Error: " + ex.Message);
+                    AgreeBox.IsEnabled = true;
+                    Install.IsEnabled = true;
+                    Cancel.IsEnabled = true;
                 }
             }
             else
